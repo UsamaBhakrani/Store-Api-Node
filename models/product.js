bf68033a0c045e4c162c315c5234c4294ae9c0fd
UsamaBhakrani <usamabhakrani@gmail.com>
2024-02-27 21:34:25 +0500

    Schema Defined

--- models/product.js
+++ models/product.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  featured: Boolean,
-  rating: Number,
+  featured: { type: Boolean, default: false },
+  rating: { type: Number, default: 4.5 },
   createdAt: { type: Date, default: Date.now },
-  name: String,
-  price: Number,
-  company: String,
+  name: { type: String, required: [true, "Name is Required"] },
+  price: { type: Number, required: [true, "Price is Required"] },
+  company: { type: String, enum: ["ikea", "liddy", "caressa", "marcos"] },
 });
 
 module.exports = mongoose.model("Product", productSchema);
