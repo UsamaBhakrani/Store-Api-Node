5966441c7409ce3d445f19fd9b42062fa8d09b51
Mohammed Usama <usamabhakrani@gmail.com>
2024-02-27 16:39:25 +0500

    BoilerPlate Setup and Connection with DB

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('04 Store API')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleWare = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+app.use(express.json());
+
+app.get("/", (req, res) =>
+  res
+    .status(200)
+    .send("<h1>Hello</h1><a href='/api/v1/products'>Products Route</a>")
+);
+
+app.use("./api/v1/products", productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleWare);
+
+const startServer = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log("Server Running on Port: ", port));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
